072cff2ebfb7aca7de13dbf329231f58672c093e
cody proo <cody20v@gmail.com>
2023-11-18 15:09:07 +0330

    Form Constants

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,34 +5,13 @@ import { useViewport } from "../../hooks";
 import { useForm } from "react-hook-form";
 import zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-const profileFormValidation = zod.object({
-   username: zod
-      .string({ required_error: "You must provide your username" })
-      .min(3, "Your username must include 3 character at least"),
-   email: zod
-      .string({ required_error: "You must provide your email address" })
-      .email({ message: "Your email address include invalid format" }),
-   password: zod
-      .string({ required_error: "You must provide your password" })
-      .min(8, "Your password must include 8 character at least"),
-   firstName: zod
-      .string({ required_error: "You must provide your first name" })
-      .min(3, "Your first name must include 8 character at least"),
-   lastName: zod
-      .string({
-         required_error: "You must provide your last name",
-      })
-      .min(3, "Your last name must include 8 character at least"),
-   age: zod.coerce
-      .number({ required_error: "You must provide your age" })
-      .min(1, "You must provide your age"),
-   bio: zod.string().optional(),
-});
+import { profileFormValidation } from "../../constant/forms";
 
 const ProfilePage: FC = () => {
-   const [img, setImg] = useState<File>();
+   const [avatar, setAvatar] = useState<File>();
    const { isDesktop } = useViewport();
+
+   const onChangeAvatar = (file?: File) => setAvatar(file);
    const { handleSubmit, control } = useForm<
       zod.infer<typeof profileFormValidation>
    >({
@@ -63,16 +42,16 @@ const ProfilePage: FC = () => {
                   <FileUploader
                      avatarHeight="120px"
                      avatarWidth="120px"
-                     file={img}
-                     onChangeFile={(file) => setImg(file)}
+                     file={avatar}
+                     onChangeFile={onChangeAvatar}
                   />
                ) : (
                   <center>
                      <FileUploader
                         avatarHeight="120px"
                         avatarWidth="120px"
-                        file={img}
-                        onChangeFile={(file) => setImg(file)}
+                        file={avatar}
+                        onChangeFile={onChangeAvatar}
                      />
                   </center>
                )}
